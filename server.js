8f3869a9689bf0acb6af29e47fad557cd621d85a
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-10-11 09:03:00 +0530

    feat:-apolloserver and typedefs and resolvers is added

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { ApolloServer, gql} = require('apollo-server')
+const { ApolloServer, gql} = require('apollo-server-express')
 const dbConfig =  require('./DB.Config/database.config')
  
 const app = express()
 require('dotenv').config();
 
 dbConfig.dbConnection();
+
+const apolloserver = new ApolloServer({
+    typeDefs:Schema,
+    resolvers:graphqlresolver,
+})
+
+apolloserver.applyMiddleware({app})
 
 app.listen(2000,()=>{console.log("server is runnig 2000")})
 
