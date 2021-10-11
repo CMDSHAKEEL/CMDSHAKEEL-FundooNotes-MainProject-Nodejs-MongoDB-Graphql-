44eb073c32b479e1894697b556f02eaae41ba1d4
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-10-11 13:16:05 +0530

    feat:-Existing user is done

--- app/WorkingoFGraphql/resolvers/userresolvers.js
+++ app/WorkingoFGraphql/resolvers/userresolvers.js
@@ -1,4 +1,5 @@
 const userModel = require('../../models/usermodel')
+const Apollerror = require('apollo-server-errors')
 const resolvers={
     Query:{
          
@@ -9,11 +10,26 @@ const resolvers={
     },
     Mutation:{
         createuser:async (_,{path})=>{
-            const {firstName,lastName,email,password} = path;
-            const post = new userModel({firstName,lastName,email,password})
-            await post.save()
-            return post;
+           // const {firstName,lastName,email,password} = path;
+            //const post = new userModel({firstName,lastName,email,password})
+           // await post.save()
+          //  return post;
+          const user = new userModel({
+              firstName:path.firstName,
+              lastName:path.lastName,
+              email:path.email,
+              password:path.password
+            })
+            const existinguser = await userModel.findOne({ email:path.email})
+            if(existinguser){
+                 return new Apollerror.UserInputError("all exist")
+            }
+              user.save();
+            return user;
+ 
+
         }
+         
     }
 }
 module.exports =resolvers
